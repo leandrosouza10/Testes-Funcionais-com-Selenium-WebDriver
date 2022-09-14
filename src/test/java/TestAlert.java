4caf83d14f053ab908e5754d4fdb748e5c9ad430
Leandro <souzaleandro79@gmail.com>
2022-09-14 15:25:53 -0300

    Test Alert

--- src/test/java/TestAlert.java
+++ src/test/java/TestAlert.java
@@ -10,7 +10,6 @@ import java.util.concurrent.TimeUnit;
 public class TestAlert {
 
     WebDriver driver = new ChromeDriver();
-
     @Test
     public void deveInteragirComAlertSimples() {
         driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
@@ -24,7 +23,6 @@ public class TestAlert {
         driver.findElement(By.id("elementosForm:nome")).sendKeys(texto);
         driver.quit();
     }
-
 
     @Test
     public void deveInteragirComAlertComfirm() {
@@ -40,11 +38,40 @@ public class TestAlert {
 
         driver.findElement(By.id("confirm")).click();
         Alert alerta = driver.switchTo().alert();
-        Assert.assertEquals("Confirm Simples", alert.getText());
-        alert.dismiss();
-        Assert.assertEquals("Negado", alert.getText());
-        alert.dismiss(); //  para cancelar um pop-up
-
+        Assert.assertEquals("Confirm Simples", alerta.getText());
+        alerta.dismiss();
+        Assert.assertEquals("Negado", alerta.getText());
+        alerta.dismiss(); //  para cancelar um pop-up
         driver.quit();
     }
+
+    @Test
+    public void deveInteragirComAlertPrompt() {
+        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("C:/seleniumAulas/testeGoogle/src/test/resources/formulario/componentes.html");
+        driver.findElement(By.id("prompt")).click();
+        Alert alert = driver.switchTo().alert();
+        Assert.assertEquals("Digite um numero",alert.getText());
+        alert.sendKeys("123456789");
+        alert.accept();
+        Assert.assertEquals("Era 123456789?",alert.getText());
+        alert.accept();
+        Assert.assertEquals(":D",alert.getText());
+        alert.accept();
+
+        //Caso não seja o esperado
+        driver.findElement(By.id("prompt")).click();
+        Alert alerta = driver.switchTo().alert();
+        Assert.assertEquals("Digite um numero",alerta.getText());
+        alerta.sendKeys("123456789");
+        alerta.accept();
+        Assert.assertEquals("Era 123456789?",alerta.getText());
+        alerta.dismiss();
+        Assert.assertEquals(":(",alerta.getText());
+        alerta.accept();
+        driver.quit();
+
+    }
+
 }
