8aadda5c66800a9d87c5b32c9c4c419492aa89ad
Leandro <souzaleandro79@gmail.com>
2022-09-14 15:50:30 -0300

    TestRadioButton

---
+++ src/test/java/RadioEndCheckBox.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class RadioEndCheckBox {
+
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+     public void deveInteragirComRadioButton(){
+        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("C:/seleniumAulas/testeGoogle/src/test/resources/formulario/componentes.html");
+        driver.findElement(By.id("elementosForm:sexo:0")).click();
+        Assert.assertTrue(driver.findElement(By.id("elementosForm:sexo:0")).isSelected());
+        driver.quit();
+    }
+}
