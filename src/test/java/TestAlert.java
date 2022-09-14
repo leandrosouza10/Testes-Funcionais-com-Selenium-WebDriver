90a4180713560c759c6ffaae5904414091248b4a
Leandro <souzaleandro79@gmail.com>
2022-09-14 10:21:08 -0300

    Test Alert finalizado

--- src/test/java/TestAlert.java
+++ src/test/java/TestAlert.java
@@ -1,4 +1,6 @@
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,19 +9,20 @@ import java.util.concurrent.TimeUnit;
 
 public class TestAlert {
 
-    WebDriver driver  = new ChromeDriver();
+    WebDriver driver = new ChromeDriver();
 
     @Test
-    public void deveInteragirComAlertSimples(){
+    public void deveInteragirComAlertSimples() {
         driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("C:/seleniumAulas/testeGoogle/src/test/resources/formulario/componentes.html");
-        driver.findElement(By.id("elementosForm")).click();
-
-
+        driver.findElement(By.id("alert")).click();
+        Alert alert = driver.switchTo().alert();
+        String texto = alert.getText();
+        Assert.assertEquals("Alert Simples", texto);
+        alert.accept();
+        driver.findElement(By.id("elementosForm:nome")).sendKeys(texto);
         driver.quit();
-
     }
-
 
 }
